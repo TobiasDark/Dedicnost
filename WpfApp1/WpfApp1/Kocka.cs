d0c1b3547e08216ff41e5e64a9f4dce4d826857d
TobiasDark <161588571+TobiasDark@users.noreply.github.com>
2024-03-19 09:19:30 +0100

    Add files via upload

---
+++ WpfApp1/WpfApp1/Kocka.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Kocka : Zvire
+    {
+        public Kocka(string barva, string jmeno, int nohy, bool letat) : base(barva, jmeno, nohy, letat)
+        {
+        }
+
+        public string Zvuk()
+        {
+            return $"Meoow";
+        }
+
+
+    }
+}
